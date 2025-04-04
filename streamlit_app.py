06963d9b52c102bb383920e60b9c3211b6e946db
Akhilsai366 <akhilsai366@gmail.com>
2025-04-04 05:56:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,5 @@ if ingredients_list:
    st.write(my_insert_stmt)
    time_to_insert = st.button('Submit Order')
    if ingredients_string:
-   session.sql(my_insert_stmt).collect()
-   st.success('Your Smoothie is ordered!', icon="✅")
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered!', icon="✅")
